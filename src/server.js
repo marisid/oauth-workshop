d8b459a804575501262e9f3c1d871ce217c2a48a
Peter Rhodes <rhodepeter@hotmail.co.uk>
2016-12-05 15:39:11 +0000

    Complete step5, nearly step6

--- src/server.js
+++ src/server.js
@@ -3,17 +3,26 @@ const Inert = require('inert');
 const Request = require('request');
 const Querystring = require('querystring');
 const Env = require('env2')('./config.env');
+const cookieAuth = require('hapi-auth-cookie');
 
 const server = new Hapi.Server();
 const port = process.env.PORT || 3000;
 
 server.connection({
   port: port
-})
+});
 
-server.register(Inert, (err) => {
-  if (err) throw err;
-})
+const options = {
+  password: process.env.ENCRYPTION_CODE,
+  cookie: 'activeUserCookie',
+  isSecure: false,
+  ttl: 24 * 60 * 60 * 1000
+};
+
+server.register([cookieAuth, Inert], (err) => {
+  if (err) {throw err};
+  server.auth.strategy('session', 'cookie', options)
+});
 
 server.route([{
   method: 'GET',
@@ -31,26 +40,49 @@ server.route([{
     const queryParams = Querystring.stringify({'client_id':process.env.CLIENT_ID, 'redirect_uri':process.env.BASE_URL + '/welcome'});
     reply.redirect(`${githubLink}?${queryParams}`);
   }
-}, {
+},{
   method: 'GET',
   path: '/welcome',
-  handler: (req, reply) => {
-    const accessTokenUrl = 'https://github.com/login/oauth/access_token';
-    const code = req.query.code;
-    Request({
-      url: accessTokenUrl,
-      method: 'POST',
-      form: {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        code: code
-      }
-    },(err,res,body) => {
-        if (err) throw err;
-        reply(body);
-    })
+  config: {
+    handler: (req, reply) => {
+      const accessTokenUrl = 'https://github.com/login/oauth/access_token';
+      Request({
+        headers: {
+          accept: 'application/json',
+        },
+        url: accessTokenUrl,
+        method: 'POST',
+        form: {
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code: req.query.code
+        }
+      },(err,res,body) => {
+          if (err) throw err;
+          const json = JSON.parse(body)
+          console.log(json.access_token);
+          const userDetails = {
+            access_token: json.access_token
+          };
+          req.cookieAuth.set(userDetails);
+          reply.redirect('/profile');
+      })
+    }
   }
-}]);
+}, {
+    method: 'GET',
+    path: '/profile',
+    config: {
+      auth: {
+        strategy: 'session'
+      },
+      handler: (req, reply) => {
+        console.log(req.auth.credentials.access_token);
+        reply('Welcome to your profile!');
+      }
+    },
+}
+]);
 
 server.start((err) => {
   if(err) throw err;
